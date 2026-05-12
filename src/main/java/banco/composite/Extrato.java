45aa14dc42628a9a398411cf8cf225c8a0f70cce
Murilo Schittino <146205147+Murilord18@users.noreply.github.com>
2026-05-12 10:42:48 -0300

    Update Extrato.Java

--- src/main/java/banco/composite/Extrato.java
+++ src/main/java/banco/composite/Extrato.java
@@ -4,7 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class Extrato  {
+public class Extrato extends Item {
+
+    private final String nome;
+    private final List<Item> itens = new ArrayList<>();
+
+    public Extrato(String nome) {
+        this.nome = nome;
+    }
+
+    public void addItem(Item item) {
+        itens.add(item);
+    }
+
+    @Override
+    public String getDescricao() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Extrato: ").append(nome).append("\n");
+        for (Item item : itens) {
+            sb.append(item.getDescricao()).append("\n");
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public double getValor() {
+        return itens.stream().mapToDouble(Item::getValor).sum();
+    } {
 
 
 }
