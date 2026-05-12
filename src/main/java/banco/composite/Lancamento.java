f693d0c82be22943275b9814cdefac45cfab4fd0
Murilo Schittino <146205147+Murilord18@users.noreply.github.com>
2026-05-12 10:45:29 -0300

    Update Lancamento.Java

--- src/main/java/banco/composite/Lancamento.java
+++ src/main/java/banco/composite/Lancamento.java
@@ -1,7 +1,26 @@
 package banco.composite;
 
 
-public class Lancamento  {
+public class Lancamento extends Item {
+
+    private final String descricao;
+    private final double valor;
+
+    public Lancamento(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    @Override
+    public String getDescricao() {
+        String tipo = valor >= 0 ? "Crédito" : "Débito";
+        return String.format("Lançamento: %s - %s: R$ %.2f", descricao, tipo, Math.abs(valor));
+    }
+
+    @Override
+    public double getValor() {
+        return valor;
+    }
 
 
 }
